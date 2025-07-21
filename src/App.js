44c8e7e362c717d525bc2d7b5dad4c7cc15a1102
7nischal <112294161+7nischal@users.noreply.github.com>
2025-07-21 19:19:01 -0300

    test 100

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          byee there testing lets see 1000!
+          byee there testing lets see 10000!
         </p>
         <a
           className="App-link"
